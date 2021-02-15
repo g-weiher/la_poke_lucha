7e27a7e1b9501507cd5ec3142a2517ffe5fce6bb
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-15 08:38:57 +0100

    get eveything in pokemonService to work

--- services/pokemonService.js
+++ services/pokemonService.js
@@ -3,21 +3,26 @@ const Pokemon = require("../models/Pokemon");
 const axios = require("axios");
 
 const pokemonService = {
-  getPokemon: () => {
-    //TODO pagination, 
-    return Pokemon.find();
+  getPokemon: async () => {
+    //TODO pagination,
+    return await Pokemon.find();
   },
   getPokemonById: async (id) => {
-    return Pokemon.find(id);
+    return await Pokemon.findOne({ id: id });
   },
   getPokemonInfoById: async (id, info) => {
-    const pokeInfo = Pokemon.find(id)[info];
-    if(info === "type") {
+    const pokemon = await Pokemon.findOne({ id: id });
+    const pokeInfo = pokemon[info];
+    if (info === "type") {
       //for every type of the pokemon create an axios request, bundle them with Promise.all and resolve them all
-      const typesResponses = await Promise.all(pokeInfo.map(type => axios.get(`https://pokeapi.co/api/v2/type/${type.toLowerCase()}`)))
+      const typesResponses = await Promise.all(
+        pokeInfo.map((type) =>
+          axios.get(`https://pokeapi.co/api/v2/type/${type.toLowerCase()}`)
+        )
+      );
       //create the return objects out of the responses and return them
-      return typesResponses.map(response => {
-        return { name: response.data.name, ...response.data.damage_relations}
+      return typesResponses.map((response) => {
+        return { name: response.data.name, ...response.data.damage_relations };
       });
     } else return pokeInfo;
   },
