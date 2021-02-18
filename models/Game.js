55be98599f9822fb000601e295502dea6038f98e
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-18 17:35:21 +0100

    populate games with pokemon

--- models/Game.js
+++ models/Game.js
@@ -2,12 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
-    winner: String,
-    looser: String,
-    turns: Number,
-    winnerScore: Number,
-    looserScore: Number,
-    date: {type: Date, default: Date.now}
+  winner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pokemon",
+    required: true,
+  },
+  looser:  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pokemon",
+    required: true,
+  },
+  turns: Number,
+  winnerScore: Number,
+  looserScore: Number,
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Game", gameSchema);
